54fa04911033b97415f770b96ef5a209417670a3
FocusedLoop <43334820+FocusedLoop@users.noreply.github.com>
2024-05-25 22:06:12 +1000

    grade level 4 working

---
+++ app.js
@@ -0,0 +1,72 @@
+// import knex to use, knex helps us talk to the database
+const configurations = require('./knexfile.js');
+const knex = require('knex')(configurations);
+const cors = require('cors');
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+//const usersRouter = require('./routes/users');
+//const knexRouter = require('./routes/knex'); 
+//const loginRouter = require('./routes/login');
+//const registerRouter = require('./routes/register');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+// Do a quick google to know WHY we need to log in the server 
+// AND it is included in the CRA
+app.use(logger('combined'));
+
+app.use((req, res, next) => {
+	req.db = knex;
+	next();
+});
+
+
+// We call this middleware. They are like plugins that help us do something to the request before the request reaches the route
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('common'));
+app.use(cors());
+
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//app.use('/me', apiRouter);
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,152 @@
+var express = require('express');
+var router = express.Router();
+
+const authorisation = require("../middleware/authorisation");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/countries', function (req, res, next) {
+    const allowedParams = ['sort'];
+    const queryParams = Object.keys(req.query);
+    
+    const invalidParams = queryParams.filter(param => !allowedParams.includes(param));
+    
+    if (invalidParams.length > 0) {
+        res.status(400).json({
+            error: true,
+            message: 'Invalid query parameters. Query parameters are not permitted.'
+        });
+    } else {
+        const sort = req.query.sort || 'asc';
+        req.db
+            .from('data')
+            .select('name', 'country')
+            .orderBy('name', sort)
+            .then((rows) => {
+                const countries = rows.map(row => row.country);
+                res.json(countries);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({ error: true, message: 'Error in MySQL query' });
+            });
+    }
+});
+
+router.get('/volcanoes', function (req, res, next) {
+    const { country, sort = 'asc' } = req.query;
+
+    if (!country) {
+        res.status(400).json({
+            error: true,
+            message: 'Country is a required query parameter.'
+        });
+        return;
+    }
+
+    req.db
+        .from('data')
+        .select('country')
+        .where('country', '=', country)
+        .then((rows) => {
+            if (rows.length === 0) {
+                res.status(400).json({
+                    error: true,
+                    message: 'Country is a required query parameter.'
+                });
+            } else {
+                req.db
+                    .from('data')
+                    .select('id', 'name', 'country', 'region', 'subregion')
+                    .where('country', '=', country)
+                    .orderBy('name', sort)
+                    .then((rows) => {
+                        res.json(rows);
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        res.status(500).json({ error: true, message: 'Error in MySQL query' });
+                    });
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: true, message: 'Error in MySQL query' });
+        });
+});
+
+router.get('/volcano/:id', function (req, res, next) {
+	// add Token error when users has been set up properly (401)
+	const allowedParams = ['sort'];
+    const queryParams = Object.keys(req.query);
+    
+    const invalidParams = queryParams.filter(param => !allowedParams.includes(param));
+    
+    if (invalidParams.length > 0) {
+        res.status(400).json({
+            error: true,
+            message: 'Invalid query parameters. Query parameters are not permitted.'
+        });
+    } else {
+    	req.db
+        	.from('data')
+        	.select('*')
+        	.where('id', '=', req.params.id)
+        	.then((rows) => {
+            	if (rows.length === 0) {
+                	res.status(404).json({
+                    	error: true,
+                    	message: 'Volcano with ID: ${volcanoId} not found.'
+                	});
+            	} else {
+                	res.json(rows);
+            	}
+        	})
+        	.catch((err) => {
+            	console.error(err);
+            	res.status(500).json({ error: true, message: 'Error in MySQL query' });
+        	});
+	}
+});
+
+router.get('/me', function (req, res, next) {
+	const me_data = {
+        name: "Joshua Wlodarczyk",
+        student_number: "n11275561"
+    };
+
+	res.json(me_data)
+});
+
+// REMOVE?
+router.post('/update', authorisation, (req, response) => { 
+	if (!req.body.data || !req.body.region || !req.body.population) {
+		response.status(400).json({ message: `Error updating population` });
+		console.log(`Error on request body:`, JSON.stringify(req.body));
+	} else {
+		const filter = {
+			Name: req.body.data,
+			Region: req.body.region,
+		};
+		const pop = {
+			Population: req.body.population,
+		};
+
+		req.db('data')
+			.where(filter)
+			.update(pop)
+			.then((_) => {
+				response.status(201).json({ message: `Successful update ${req.body.data}` });
+				console.log(`successful population update:`, JSON.stringify(req.body));  // server logging
+			})
+			.catch((error) => {
+				console.error(`Error updating population:`, error.message);  // server logging
+				response.status(500).json({ message: 'Database error - not updated' });
+			});
+	}
+});
+
+module.exports = router;
